b0bfcfb434d7425ffe3d4d498c68a7bb366c3ee0
Emmanuel <emmason512@gmail.com>
2024-08-07 10:20:59 +0100

    New file

---
+++ OurBioData.sh
@@ -0,0 +1,10 @@
+#!/bin/bash'
+read -p "Below are my Bio Datas for me and my Group mate, Tap the enter key to view details"
+read -p "NAME: Emmanuel and Goodness"
+read -p "AGE: 20yrs and 18yrs"
+read -p "DATE OF BIRTH: 25/09/2004 and 12/07/2006"
+read -p "COUNTRY: Nigerians"
+read -p "STATE OF ORIGIN:Both are from Benue State"
+read -p "LGA:Both are from  Otukpo"
+echo "SUMMARY: Emmanuel is 20yrs old, he was born on the 25th September, 2004 in Otukpo, Benue, Nigeria, while Goodness is 18yrs old, born on the 12th july, 2006 in Otukpo, Benue, Nigeria."
+ 
